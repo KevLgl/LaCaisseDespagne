a5e40e3428343071f09b2a0668cedfdb7f16bb0b
Kevin Laguel <laguel.kev.pro@gmail.com>
2020-09-30 11:19:27 +0200

    add img in header

--- template/header.php
+++ template/header.php
@@ -1,9 +1,9 @@
 
 <header>
-      <div class="jumbotron jumbotron-fluid">
+      <!-- <div class="jumbotron jumbotron-fluid">
         <div class="container">
-          <h1 class="display-4">LE HEADER</h1>
+          <h1 class="display-4"></h1>
           <p class="lead">NOTRE SLOGAN</p>
         </div>
-      </div>
-    </header>
+      </div> -->
+</header>
