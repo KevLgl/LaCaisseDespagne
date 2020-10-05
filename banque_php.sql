53e544ea0c71a40d4ce1957973deb4742ecc0b06
Kevin Laguel <laguel.kev.pro@gmail.com>
2020-10-05 09:59:54 +0200

    add the second bdd

--- banque_php.sql
+++ banque_php.sql
@@ -1,60 +1,66 @@
--- Delete existing database and user
 DROP DATABASE IF EXISTS banque_php;
-DROP USER IF EXISTS 'banquePHP'@'*';
-
--- Create database with utf-8 characters
 CREATE DATABASE banque_php CHARACTER SET 'utf8';
+USE banque_php;
 
--- Create user and his privileges
-CREATE USER 'banquePHP'@'*' IDENTIFIED BY 'mdp';
-GRANT ALL ON banque_php.* TO 'banquePHP'@'*';
-
-
--- create 3 tables in database
-
-CREATE TABLE user (
-  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  nom varchar(50) NOT NULL,
-  email varchar(30) NOT NULL,
-  pass varchar(30) NOT NULL,
-);
-
-CREATE TABLE personal_account (
-  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  account_type varchar(30) NOT NULL,
-  numb decimal NOT NULL,
-  opening_date date NOT NULL,
-  amount int NULL
-);
+DROP USER IF EXISTS 'BanquePHP'@'Localhost';
+CREATE USER 'BanquePHP'@'Localhost';
+GRANT ALL PRIVILEGES ON banque_php.* To 'BanquePHP'@'Localhost' IDENTIFIED BY 'banque76';
 
 
-CREATE TABLE operations (
-  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  amout int NULL,
-  compte_id int NOT NULL,
-  debtor_account_id int NOT NULL,
-  creditor_account_id int NOT NULL
-);
+CREATE TABLE User(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  lastname VARCHAR(50) NOT NULL,
+  firstname VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  city VARCHAR(30) NOT NULL,
+  city_code CHAR(5) NOT NULL,
+  adress VARCHAR(50) NOT NULL,
+  sex CHAR(1) NOT NULL,
+  password VARCHAR(255) NOT NULL UNIQUE,
+  birth_date DATE NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE=InnoDB;
 
+INSERT INTO User(lastname, firstname, email, city, city_code, adress, sex, password, birth_date)
+VALUES
+("Dupont", "Richard", "r.dupont@gmail.com", "Rouen", "76100", "9 rue du gros horloge", "h", "Riri1962!", "1962-05-21"),
+("Melez", "Claire", "clairemelez@outlook.com", "Lille", "59100", "45 rue du Molinel", "f", "AstraGirl154", "1989-11-14");
 
+CREATE TABLE Account(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  amount DECIMAL(11,2) NOT NULL,
+  opening_date TIMESTAMP NOT NULL,
+  account_type VARCHAR(50) NOT NULL,
+  user_id INT UNSIGNED,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES User(id)
+)
+ENGINE=InnoDB;
 
--- add foreign keys for table personal_account
-ALTER TABLE personal_account
-  ADD CONSTRAINT user_id 
-  FOREIGN KEY(user) 
-  REFERENCES user(id);
--- add foreign keys for table opération
-ALTER TABLE operations
-  ADD CONSTRAINT user_id 
-  FOREIGN KEY(user) 
-  REFERENCES user(id);
+INSERT INTO Account(amount, opening_date, account_type, user_id)
+VALUES
+(596.23, NOW(), "Compte courant", 1),
+(12345, NOW(), "Livret A", 1),
+(500, NOW(), "PEL", 1),
+(-56.78, NOW(), "Compte courant", 2);
 
+CREATE TABLE Operation(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  operation_type VARCHAR(30) NOT NULL,
+  amount DECIMAL(11,2) NOT NULL,
+  registered TIMESTAMP NOT NULL,
+  label VARCHAR(50) NULL,
+  account_id INT UNSIGNED,
+  PRIMARY KEY (id),
+  FOREIGN KEY (account_id) REFERENCES Account(id)
+)
+ENGINE=InnoDB;
 
--- add of some users, accounts and operations in the tables
-INSERT INTO user (name, email, pass)
-  VALUE ('John Doe', 'johndoe@a.fr', 'coucou'),
-        ('Jule Verne', 'az@afpa.fr', 'coucou1');
-
-INSERT INTO personal_account (account_type, numb, opening_date, amount, user_id)
-  VALUE ('compte courant', 1402176513, '2020-10-02', 165,4),
-        ('PEL', 30092785114, '2019-01-03', 12);
+INSERT INTO Operation(operation_type, amount, registered, label, account_id)
+VALUES
+("débit", -15.60, NOW(), "le poulet d'or SARL", 1),
+("crédit", 500, NOW(), NULL, 2),
+("débit", -7.62, NOW(), "carrefour essence", 2),
+("débit", -50, NOW(), "frais de gestion", 2)
+;
---
+++ banque_phpbis.sql
@@ -0,0 +1,62 @@
+-- Delete existing database and user
+DROP DATABASE IF EXISTS banque_php;
+DROP USER IF EXISTS 'banquePHP'@'*';
+
+-- Create database with utf-8 characters
+CREATE DATABASE banque_php CHARACTER SET 'utf8';
+
+-- Create user and his privileges
+CREATE USER 'banquePHP'@'*' IDENTIFIED BY 'mdp';
+GRANT ALL ON banque_php.* TO 'banquePHP'@'*';
+
+
+-- create 3 tables in database
+CREATE TABLE user (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  firstname varchar(50) NOT NULL,
+  lastname varchar(50) NOT NULL,
+  city varchar(30) NOT NULL,
+  city_code int NOT NULL,
+  email varchar(30) NOT NULL UNIQUE,
+  pass varchar(30) NOT NULL,
+);
+
+CREATE TABLE personal_account (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  account_type varchar(30) NOT NULL,
+  numb decimal NOT NULL,
+  opening_date date NOT NULL,
+  amount int NULL,
+);
+
+
+CREATE TABLE operations (
+  id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  amout int NULL,
+  compte_id int NOT NULL,
+  debtor_account_id int NOT NULL,
+  creditor_account_id int NOT NULL,
+);
+
+
+
+-- add foreign keys for table personal_account
+ALTER TABLE personal_account
+  ADD CONSTRAINT fk_user_id 
+  FOREIGN KEY(user) 
+  REFERENCES user(id);
+-- add foreign keys for table opération
+ALTER TABLE operations
+  ADD CONSTRAINT fk_user_id 
+  FOREIGN KEY(user) 
+  REFERENCES user(id);
+
+
+-- add of some users, accounts and operations in the tables
+INSERT INTO user (name, email, pass)
+  VALUE ('John Doe', 'johndoe@a.fr', 'coucou'),
+        ('Jule Verne', 'az@afpa.fr', 'coucou1');
+
+INSERT INTO personal_account (account_type, numb, opening_date, amount, user_id)
+  VALUE ('compte courant', 1402176513, '2020-10-02', 165, 4),
+        ('PEL', 30092785114, '2019-01-03',12 );
